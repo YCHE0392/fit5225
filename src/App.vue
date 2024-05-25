56af8a89f100344ffbcab44e5b51f7ddc5d0ec77
YCHE0392 <112544865+YCHE0392@users.noreply.github.com>
2024-05-25 21:22:53 +1000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
-<script >
+<script setup lang="ts">
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
-import UploadImage from './components/UploadImage.vue'
 </script>
 
 <template>
@@ -9,7 +8,6 @@ import UploadImage from './components/UploadImage.vue'
     <authenticator>
       <template v-slot="{ user, signOut }">
         <h1>Hello {{user?.signInDetails?.loginId}}'s todos</h1>
-        
         <button @click="signOut">Sign Out</button>
       </template>
     </authenticator>
